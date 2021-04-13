b219b9329cd12651f74225f71b22534881d582bf
Teamer Tibebu <teamertibebu@gmail.com>
2021-04-13 14:31:31 -0500

    (update) NavBar to handle screen resizing more smoothly

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Button } from '@material-ui/core/';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+
+const useStyles = makeStyles((styles) => ({
+  profileImage: {
+    width: '100%',
+    height: '100%',
+  },
+  imageContainer: {
+    width: '25%',
+    height: '25%',
+  },
+}));
 
 async function loginUser(credentials) {
   return fetch('http://localhost:8080/login', {
@@ -14,9 +24,20 @@ async function loginUser(credentials) {
   }).then((data) => data.json());
 }
 
+async function createUserAccount(info) {
+  return axios
+    .post('http://localhost:8080/createAccount', info)
+    .then(({ data }) => data);
+}
+
 const Login = ({ setToken }) => {
-  const [username, setUserName] = useState();
+  const [username, setUserName] = useState('@');
   const [password, setPassword] = useState();
+  const [email, setEmail] = useState();
+  const [image, setImage] = useState();
+  const [userAvailable, setUserAvailable] = useState(true);
+
+  const classes = useStyles();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -25,25 +46,62 @@ const Login = ({ setToken }) => {
       password,
     });
     setToken(token);
+
+    const haha = await createUserAccount({ username, password, email, image });
+    setUserAvailable(haha.available);
+    setUserAvailable(true);
+    console.log(haha);
+  };
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    setImage(URL.createObjectURL(file));
   };
 
   return (
     <div className="login-wrapper">
-      <h1>Please Login</h1>
+      <h1>Create Account</h1>
       <form onSubmit={handleSubmit}>
         <label>
           <p>Username</p>
-          <input type="text" onChange={(e) => setUserName(e.target.value)} />
+          <input
+            type="text"
+            required
+            onInvalid={() => "alert('You must fill out the form!')"}
+            onChange={(e) => setUserName(e.target.value)}
+          />
         </label>
         <label>
           <p>Password</p>
           <input
             type="password"
+            required
             onChange={(e) => setPassword(e.target.value)}
           />
         </label>
+        <label>
+          <p>E-Mail</p>
+          <input
+            required
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <div className={classes.imageContainer}>
+          <img src={image} alt="" className={classes.profileImage} />
+        </div>
+        <label>
+          <p>Profile Image</p>
+          <input type="file" onChange={handleImageUpload} />
+        </label>
         <div>
-          <button type="submit">Submit</button>
+          {userAvailable ? (
+            <button type="submit">Submit</button>
+          ) : (
+            <button type="submit" disabled>
+              Submit
+            </button>
+          )}
         </div>
       </form>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import {
   Typography,
   Menu,
   MenuItem,
+  Hidden,
 } from '@material-ui/core';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import FavoriteIcon from '@material-ui/icons/Favorite';
@@ -15,11 +16,15 @@ import HomeIcon from '@material-ui/icons/Home';
 import EmailIcon from '@material-ui/icons/Email';
 import SearchIcon from '@material-ui/icons/Search';
 import AddBoxIcon from '@material-ui/icons/AddBox';
+import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
 
 const Navbar = ({ clearToken }) => {
   const [anchorEl, setAnchorEl] = useState(null);
+  const [menuBarAnchor, setMenuBarAnchor] = useState();
+
   const open = Boolean(anchorEl);
+  const openMenu = Boolean(menuBarAnchor);
 
   const handleAccount = (event) => {
     setAnchorEl(event.currentTarget);
@@ -31,6 +36,14 @@ const Navbar = ({ clearToken }) => {
 
   const handleSignOut = () => {
     clearToken();
+  };
+
+  const handleMenuBar = (e) => {
+    setMenuBarAnchor(e.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setMenuBarAnchor(null);
   };
 
   const useStyles = makeStyles((theme) => ({
@@ -68,6 +81,7 @@ const Navbar = ({ clearToken }) => {
       display: 'flex',
     },
     icons: {
+      minWidth: '200px',
       paddingTop: '7px',
       alignItems: 'center',
       justifyContent: 'space-around',
@@ -81,12 +95,14 @@ const Navbar = ({ clearToken }) => {
       <Toolbar>
         <IconButton className={classes.root} disableRipple>
           <Grid container spacing={10} className={classes.container}>
-            <Grid item xs={4}>
-              <Typography variant="h6" noWrap className={classes.typo}>
-                Instaclone
-              </Typography>
-            </Grid>
-            <Grid item xs={4} className={classes.search}>
+            <Hidden smDown>
+              <Grid item lg={2} md={2} sm={3} xs={3}>
+                <Typography variant="h6" noWrap className={classes.typo}>
+                  Instaclone
+                </Typography>
+              </Grid>
+            </Hidden>
+            <Grid item lg={5} md={5} sm={6} xs={6} className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
@@ -96,23 +112,37 @@ const Navbar = ({ clearToken }) => {
                 className={classes.inputInput}
               />
             </Grid>
-            <Grid item xs={4} className={classes.iconContainer}>
+            <Grid
+              item
+              lg={5}
+              md={5}
+              sm={6}
+              xs={3}
+              className={classes.iconContainer}
+            >
               <Grid container spacing={0} className={classes.icons}>
-                <Grid item xs={1}>
-                  <AddBoxIcon />
-                </Grid>
-                <Grid item xs={1}>
-                  <HomeIcon />
-                </Grid>
-                <Grid item xs={1}>
-                  <FavoriteIcon />
-                </Grid>
-                <Grid item xs={1}>
-                  <EmailIcon />
-                </Grid>
-                <Grid item xs={1}>
-                  <AccountCircleIcon onClick={handleAccount} />
-                </Grid>
+                <Hidden smDown>
+                  <Grid item xs={1}>
+                    <AddBoxIcon />
+                  </Grid>
+                  <Grid item xs={1}>
+                    <HomeIcon />
+                  </Grid>
+                  <Grid item xs={1}>
+                    <FavoriteIcon />
+                  </Grid>
+                  <Grid item xs={1}>
+                    <EmailIcon />
+                  </Grid>
+                  <Grid item xs={1}>
+                    <AccountCircleIcon onClick={handleAccount} />
+                  </Grid>
+                </Hidden>
+                <Hidden mdUp>
+                  <Grid item>
+                    <MenuIcon onClick={handleMenuBar} />
+                  </Grid>
+                </Hidden>
               </Grid>
             </Grid>
           </Grid>
@@ -133,6 +163,40 @@ const Navbar = ({ clearToken }) => {
             <MenuItem onClick={handleAcctClose}>Profile</MenuItem>
             <MenuItem onClick={handleAcctClose}>My Account</MenuItem>
             <MenuItem onClick={handleSignOut}>Sign Out</MenuItem>
+          </Menu>
+          <Menu
+            anchorEl={menuBarAnchor}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            open={openMenu}
+            onClose={handleMenuClose}
+          >
+            <MenuItem>
+              <AddBoxIcon />
+              Post
+            </MenuItem>
+            <MenuItem>
+              <HomeIcon />
+              Home
+            </MenuItem>
+            <MenuItem>
+              <FavoriteIcon />
+              Likes
+            </MenuItem>
+            <MenuItem>
+              <EmailIcon /> Messages
+            </MenuItem>
+            <MenuItem>
+              <AccountCircleIcon />
+              Account
+            </MenuItem>
           </Menu>
         </IconButton>
       </Toolbar>
