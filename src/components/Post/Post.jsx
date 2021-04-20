a072ec3bc527f1bf1e5298c51d968c89bdc22317
Teamer Tibebu <teamertibebu@gmail.com>
2021-04-20 14:12:01 -0500

    (update) post component to have proper styling. Images fill containers & post header remains centered.

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,6 +1,24 @@
 import './Post.css';
 import useStyles from './PostStyle';
-import { Grid, Avatar, Typography } from '@material-ui/core';
+import {
+  Grid,
+  Avatar,
+  Typography,
+  createMuiTheme,
+  ThemeProvider,
+} from '@material-ui/core';
+
+const theme = createMuiTheme({
+  overrides: {
+    // MuiGrid: {
+    //   'spacing-xs-1': {
+    //     margin: 0,
+    //     color: 'red',
+    //     backgroundColor: 'purple',
+    //   },
+    // },
+  },
+});
 
 const Post = ({ post }) => {
   const classes = useStyles();
@@ -12,8 +30,8 @@ const Post = ({ post }) => {
 
   return (
     <Grid container spacing={2} direction="column" className={classes.post}>
-      <Grid container item xs={12} className={classes.postHeader}>
-        <Grid container item xs={5}>
+      <Grid container item xs={12}>
+        <Grid container item xs={5} className={classes.postHeader}>
           <Grid className={classes.avatar}>
             <Avatar src={image} alt="" />
           </Grid>
@@ -25,10 +43,9 @@ const Post = ({ post }) => {
         </Grid>
         <Grid xs={7}></Grid>
       </Grid>
-
-      <Grid item xs={12} className={classes.imageContainer}>
+      <div xs={12} className={classes.imageContainer}>
         <img className={classes.img} alt="post" src={image} />
-      </Grid>
+      </div>
       <Grid item xs={12}>
         <div className="Post-caption">
           <strong>{caption}</strong>
--- src/components/Post/PostStyle.js
+++ src/components/Post/PostStyle.js
@@ -1,26 +1,21 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createMuiTheme } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core/';
 
 const useStyles = makeStyles((theme) => ({
   post: {
     border: '1px solid lightgrey',
     marginBottom: '15%',
   },
-  imageContainer: {
-    width: '100%',
-  },
   img: {
     width: '100%',
-    margin: '2% 0 0 0',
   },
-
   avatar: {
     position: 'relative',
-    top: '15px',
     padding: '0 3% 0 5%',
   },
   username: {
     position: 'relative',
-    top: '24px',
+    top: '20%',
   },
 }));
 
